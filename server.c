52e21322ad9c917ddeefd6a54ecba5fe5407cd62
root <root@localhost.localdomain>
2017-01-14 12:45:41 +0100

    primo commit

---
+++ server.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define MAX_L 16
+
+int s1,s2;
+char buffer[MAX_L];
+char str[MAX_L];
+
+void server_connection()
+{
+	struct sockaddr_in server_endpoint, client_endpoint;
+	server_endpoint.sin_family=AF_INET;
+	server_endpoint.sin_port=htons(1025);
+	server_endpoint.sin_addr.s_addr=INADDR_ANY;
+	s1=socket(AF_INET,SOCK_STREAM,0);
+	bind(s1,&server_endpoint,sizeof(server_endpoint));
+	listen(s1,10);
+	s2=accept(s1,&client_endpoint,sizeof(client_endpoint));
+	printf("connessione eseguita! :D \n");
+}
+
+void server_close()
+{
+	close(s2);
+	close(s1);
+}
+
+void server_send()
+{
+	strcpy(str, "server: ciao");
+	send(s2, str, 15, 0);
+}
+
+void server_recovery()
+{
+	if(recv(s1,buffer[MAX_L],MAX_L,0)!=0)
+	{
+		int n=recv(s1,buffer[MAX_L],MAX_L,0);
+		//printf("Risposta ricevuta \"%s\"\n",buffer);
+		puts(buffer);
+	}
+	else
+	{
+		printf("Messaggio non ricevuto :( \n");
+	}
+}
+
+int main()
+{
+	server_connection();
+	server_recovery();
+	server_close();
+	
+	return 0;
+}
